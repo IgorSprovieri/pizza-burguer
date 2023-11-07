c41419d64e03f28c57975c293c33368c0b0a048e
Igor Sprovieri <igsp1@hotmail.com>
2023-11-06 21:26:12 -0300

    Refactor: Db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,5 @@
 import { Pool } from "pg";
 
-const {
-  NEXT_PUBLIC_DB_USER,
-  NEXT_PUBLIC_DB_HOST,
-  NEXT_PUBLIC_DB_DATABASE,
-  NEXT_PUBLIC_DB_PASSWORD,
-  NEXT_PUBLIC_DB_PORT,
-} = process.env;
-
 export const db = new Pool({
-  user: NEXT_PUBLIC_DB_USER,
-  host: NEXT_PUBLIC_DB_HOST,
-  database: NEXT_PUBLIC_DB_DATABASE,
-  password: NEXT_PUBLIC_DB_PASSWORD,
-  port: Number(NEXT_PUBLIC_DB_PORT),
+  connectionString: process.env.NEXT_PUBLIC_DB_URL,
 });

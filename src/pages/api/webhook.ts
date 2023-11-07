1a52bc36df693dd0eca80d684d259a1d76bb2952
Igor Sprovieri <igsp1@hotmail.com>
2023-11-07 16:36:12 -0300

    Try catch on webhook

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -4,25 +4,22 @@ import { Order } from "@/types";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const startOrder = async (username: string): Promise<void> => {
-  await orderRepository.createOrder(username);
-
   await sendMessage(username, `Olá, aqui é o pizza burguer`);
   await sendMessage(username, `Escolha os items do seu pedido no link abaixo:`);
   await sendMessage(
     username,
     `${process.env.NEXT_PUBLIC_BASE_URL}/${username}`
   );
+
+  await orderRepository.createOrder(username);
 };
 
 const payMethodResponse = async (username: string): Promise<void> => {
-  await orderRepository.updateStage(username, "adress");
-
   await sendMessage(username, `E qual é o endereço de entrega?`);
+  await orderRepository.updateStage(username, "adress");
 };
 
 const adressResponse = async (username: string): Promise<void> => {
-  await orderRepository.updateStage(username, "finish");
-
   await sendMessage(
     username,
     `Seu pedido está sendo preparado e chegará em torno de 30 minutos`
@@ -31,6 +28,8 @@ const adressResponse = async (username: string): Promise<void> => {
     username,
     `Estou finalizando nossa conversa, qualquer dúvida pode nos ligar`
   );
+
+  await orderRepository.updateStage(username, "finish");
 };
 
 export default async function handler(
@@ -38,28 +37,34 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    type Body = { message: string; username: string };
-    const { message, username }: Body = req.body;
+    try {
+      type Body = { message: string; username: string };
+      const { message, username }: Body = req.body;
 
-    const rows = await orderRepository.getOrder(username);
+      const rows = await orderRepository.getOrder(username);
 
-    const found: Order = rows[0];
+      const found: Order = rows[0];
 
-    if (!found) {
-      if (message === "Olá, desejo fazer um pedido") {
-        await startOrder(username);
+      if (!found) {
+        if (message === "Olá, desejo fazer um pedido") {
+          await startOrder(username);
+        }
+        return res.status(200).json({ success: true });
       }
-      return res.status(200).json({ success: true });
-    }
 
-    if (found.stage === "payMethod") {
-      await payMethodResponse(username);
-      return res.status(200).json({ success: true });
-    }
+      if (found.stage === "payMethod") {
+        await payMethodResponse(username);
+        return res.status(200).json({ success: true });
+      }
 
-    if (found.stage === "adress") {
-      await adressResponse(username);
+      if (found.stage === "adress") {
+        await adressResponse(username);
+        return res.status(200).json({ success: true });
+      }
+
       return res.status(200).json({ success: true });
+    } catch (error: any) {
+      return res.status(500).json({ error: error?.message });
     }
   }
 
--- src/services/wpp/index.ts
+++ src/services/wpp/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const sendMessage = async (username: string, message: string) => {
+  console.log(username, message);
   await axios.post(`${process.env.NEXT_PUBLIC_WPP_API_URL}/send`, {
     username: username,
     message: message,

1d801127883d9e4e94fa518f24311bb952198cb3
Igor Sprovieri <igsp1@hotmail.com>
2023-11-06 21:16:40 -0300

    Feat: DB env variables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,17 @@
 import { Pool } from "pg";
 
+const {
+  NEXT_PUBLIC_DB_USER,
+  NEXT_PUBLIC_DB_HOST,
+  NEXT_PUBLIC_DB_DATABASE,
+  NEXT_PUBLIC_DB_PASSWORD,
+  NEXT_PUBLIC_DB_PORT,
+} = process.env;
+
 export const db = new Pool({
-  user: "docker",
-  host: "localhost",
-  database: "docker",
-  password: "docker",
-  port: 5432,
+  user: NEXT_PUBLIC_DB_USER,
+  host: NEXT_PUBLIC_DB_HOST,
+  database: NEXT_PUBLIC_DB_DATABASE,
+  password: NEXT_PUBLIC_DB_PASSWORD,
+  port: Number(NEXT_PUBLIC_DB_PORT),
 });
